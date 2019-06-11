2942756112caed3973490ed8d0b2258686087dad
Shubhanshu Kumar <shubhanshu12@gmail.com>
2019-06-12 01:20:25 +0530

    Add check balance API

--- src/main/java/com/shubhanshu/paytmdemo/controller/WalletController.java
+++ src/main/java/com/shubhanshu/paytmdemo/controller/WalletController.java
@@ -1,14 +1,17 @@
 package com.shubhanshu.paytmdemo.controller;
 
 import com.shubhanshu.paytmdemo.dto.*;
+import com.shubhanshu.paytmdemo.model.PaytmUser;
 import com.shubhanshu.paytmdemo.model.TransactionLog;
 import com.shubhanshu.paytmdemo.model.Wallet;
+import com.shubhanshu.paytmdemo.service.UserService;
 import com.shubhanshu.paytmdemo.service.WalletService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
 @RestController
@@ -16,9 +19,11 @@ import javax.ws.rs.POST;
 public class WalletController {
 
     private WalletService walletService;
+    private UserService userService;
 
-    public WalletController(WalletService walletService) {
+    public WalletController(WalletService walletService, UserService userService) {
         this.walletService = walletService;
+        this.userService = userService;
     }
 
     @POST
@@ -63,5 +68,20 @@ public class WalletController {
         } catch (Exception e) {
             return new ErrorDTO(e.getMessage(), e.toString());
         }
+    }
+
+    @GET
+    @RequestMapping("/checkBalance")
+    public Long checkBalance(@RequestParam Long phoneNumber){
+        if(phoneNumber == null) {
+            return null;
+        }
+
+        PaytmUser user = userService.findByPhoneNumber(phoneNumber);
+        Wallet userWallet = user.getWallet();
+        if(userWallet == null) {
+            return null;
+        }
+        return userWallet.getMoney();
     }
 }
